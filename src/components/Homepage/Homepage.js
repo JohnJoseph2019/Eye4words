99081eed53ae1f624f45f66a9a29a7e72c36085f
Juan Delgado <Delgado-juan@hotmail.com>
2020-04-29 02:19:22 -0400

    Change the content list of instructions and added a .css file

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -1,19 +1,20 @@
 import React from "react"
 import { Link } from "react-router-dom"
+import "./Homepage.css"
 
 
 export default function Homepage(props) {
   console.log('Homepage', props)
   return (
-    <>
+    <div className="homepage">
       <h1>Anagrams</h1>
       <ol>
-        <li>You will get 7 letters</li>
-        <li>You have 30sec. to type as many words as you can</li>
+        <li>Come up with as many words you can</li>
+        <li>A word has to be atleast 3 characters long</li>
         <li>To submit an answer just click the submit button or press you enter key</li>
         <li>Okay click 'Ready' to Begin</li>
       </ol>
-      <Link to="/PlayGame" > <button onClick={props.handleRandomPick}> Ready </button></Link>
-    </>
+      <Link to="/PlayGame" > <button className="ready" onClick={props.handleRandomPick}> Ready </button></Link>
+    </div>
   )
 }
