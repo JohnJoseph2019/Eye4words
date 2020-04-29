e12fe5d95b29617b29ed2eee11b5be89ecf5a893
Juan Delgado <Delgado-juan@hotmail.com>
2020-04-29 11:30:53 -0400

    Refactor some syntax wording to make sense when passing the any fucntion from any compoenent

--- src/components/RestartButton/RestartButton.js
+++ src/components/RestartButton/RestartButton.js
@@ -1,10 +1,9 @@
 import React from "react"
-import { Link } from "react-router-dom"
 
 export default function RestartButton(props) {
   console.log('RestartButton', props)
   return (
-    <button onClick={props.handleTimer}>
+    <button className="restartButton" onClick={props.handleRestart}>
       Restart
     </button>
   )
