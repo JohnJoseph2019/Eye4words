f369bddcc1d16c429a9da478de25984c1bd7d128
Juan Delgado <Delgado-juan@hotmail.com>
2020-04-29 18:55:59 -0400

    Addded a table component for point system info to e display in he Homepage right now

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { Link } from "react-router-dom"
 import "./Homepage.css"
+import PointSystem from "../PointSystem/PointSystem"
 
 
 export default function Homepage(props) {
@@ -12,7 +13,7 @@ export default function Homepage(props) {
         <li>Giving a string of characters come up with as many words that you can</li>
         <li>A word has to be atleast 3 characters long</li>
         <li>To submit an answer just click the submit button or press you enter key</li>
-        <li>Okay click 'Ready' to Begin</li>
+        <li><PointSystem /></li>
       </ol>
       <Link to="/PlayGame" > <button className="ready" onClick={props.handleRandomPick}> Ready </button></Link>
     </div>
---
+++ src/components/PointSystem/PointSystem.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import "./PointSystem.css"
+
+export default function PointSystem() {
+  return (
+    <>
+      <table>
+        <tr>
+          <th>3 Letter Word</th>
+          <th>4 Letter Word</th>
+          <th>5 Letter Word</th>
+          <th>6 Letter Word</th>
+          <th>7 Letter Word!!!!</th>
+        </tr>
+        <tr>
+          <td>100pt</td>
+          <td>200pt</td>
+          <td>300pt</td>
+          <td>400pt</td>
+          <td>1000pt</td>
+        </tr>
+      </table>
+    </>
+  )
+}
