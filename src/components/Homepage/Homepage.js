303107be7cb291a41fc338733ba5d20512966e54
Juan Delgado <Delgado-juan@hotmail.com>
2020-04-29 17:45:14 -0400

    refactor and change some syntax

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -7,9 +7,9 @@ export default function Homepage(props) {
   console.log('Homepage', props)
   return (
     <div className="homepage">
-      <h1>Anagrams</h1>
+      <h1>Let's mine for words</h1>
       <ol>
-        <li>Come up with as many words you can</li>
+        <li>Giving a string of characters come up with as many words that you can</li>
         <li>A word has to be atleast 3 characters long</li>
         <li>To submit an answer just click the submit button or press you enter key</li>
         <li>Okay click 'Ready' to Begin</li>
