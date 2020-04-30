6b9d8c46c75f21cd090a7fb708f2f798044ee136
Juan Delgado <Delgado-juan@hotmail.com>
2020-04-30 13:43:33 -0400

    Finish testing the App test

--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import { render } from '@testing-library/react';
 import App from './App';
-import { BrowserRouter } from "react-router-dom"
 import { shallow } from 'enzyme';
 
 
-test('renders learn react link', () => {
-  const { getByText } = render(<BrowserRouter> <App /> </BrowserRouter>);
-  const linkElement = getByText('learn React');
-  expect(linkElement).toBeInTheDocument();
+test('App Componentw', () => {
+  const wrapper = shallow(<App />)
+  expect(wrapper.exists('.App')).toBe(true);
 });
