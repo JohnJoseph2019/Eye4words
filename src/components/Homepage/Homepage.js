1a262a77d568ed2b7fccfbfa2a55d7a5b4ebd298
Juan Delgado <Delgado-juan@hotmail.com>
2020-04-30 01:32:05 -0400

    Try to add styling not looking good. I edited homepage and PlayGame file . I did styling and in PlayGame file I did an animation

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -8,13 +8,9 @@ export default function Homepage(props) {
   console.log('Homepage', props)
   return (
     <div className="homepage">
-      <h1>Let's mine for words</h1>
-      <ol>
-        <li>Giving a string of characters come up with as many words that you can</li>
-        <li>A word has to be atleast 3 characters long</li>
-        <li>To submit an answer just click the submit button or press you enter key</li>
-        <li><PointSystem /></li>
-      </ol>
+      <h1>Eye4Words</h1>
+      <h2>Find as many words as you can in nunder 30sec</h2>
+      <PointSystem />
       <Link to="/PlayGame" > <button className="ready" onClick={props.handleRandomPick}> Ready </button></Link>
     </div>
   )
