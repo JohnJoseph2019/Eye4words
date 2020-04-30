ba6aaf82fd87524e56da3f848ec9d90b072f18b1
Juan Delgado <Delgado-juan@hotmail.com>
2020-04-30 08:08:41 -0400

    Added elements that work with react thead /tbody

--- src/components/PointSystem/PointSystem.js
+++ src/components/PointSystem/PointSystem.js
@@ -5,21 +5,26 @@ export default function PointSystem() {
   return (
     <>
       <table>
-        <tr>
-          <th>3 Letter Word</th>
-          <th>4 Letter Word</th>
-          <th>5 Letter Word</th>
-          <th>6 Letter Word</th>
-          <th>7 Letter Word!!!!</th>
-        </tr>
-        <tr>
-          <td>100pt</td>
-          <td>200pt</td>
-          <td>300pt</td>
-          <td>400pt</td>
-          <td>1000pt</td>
-        </tr>
+        <thead>
+          <tr>
+            <th>3 Letter Word</th>
+            <th>4 Letter Word</th>
+            <th>5 Letter Word</th>
+            <th>6 Letter Word</th>
+            <th>7 Letter Word!!!!</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>100pt</td>
+            <td>200pt</td>
+            <td>300pt</td>
+            <td>400pt</td>
+            <td>1000pt</td>
+          </tr>
+        </tbody>
       </table>
     </>
   )
 }
+//Why do I really need add thead for my header table and tboyd for my data
