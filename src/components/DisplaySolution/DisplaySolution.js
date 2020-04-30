f007f9d11b8da6c684486871cf7b38877494c4c1
Juan Delgado <Delgado-juan@hotmail.com>
2020-04-30 08:05:33 -0400

    Added css file and made the div for list of solution

--- src/components/DisplaySolution/DisplaySolution.js
+++ src/components/DisplaySolution/DisplaySolution.js
@@ -1,17 +1,18 @@
 import React from "react"
 import { Redirect } from "react-router-dom"
+import "./DisplaySolution.css"
 
 export default function DisplaySolution(props) {
   console.log('DisplaySolution', props)
   return (
-    <>
+    <div className="solutionList">
+      <h3>Solution</h3>
       {props.solution.length > 0 ?
-        <div>
-          <h3>Solution</h3>
+        <div className="overflowSolution">
           {props.solution.map(word =>
             <div key={word} className="resultList">{word}</div>)
           }</div> : <Redirect to="/" />}
-    </>
+    </div>
 
   )
 }
