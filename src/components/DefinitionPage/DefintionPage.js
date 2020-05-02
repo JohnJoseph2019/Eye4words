c60980f4646112e18765757c7704c1e492f12516
Juan Delgado <Delgado-juan@hotmail.com>
2020-05-02 18:00:41 -0400

    Soleil help with my warning .. Please when calling API render page first then make useFeect to call api and add one [] for a one time call

--- src/components/DefinitionPage/DefintionPage.js
+++ src/components/DefinitionPage/DefintionPage.js
@@ -1,27 +1,31 @@
-import React, { useState } from "react"
-import { Link, useParams } from "react-router-dom"
+import React, { useState, useEffect } from "react"
+import { Link, useParams, NavLink } from "react-router-dom"
 import axios from "axios"
 import "./DefinitionPage.css"
 
 
 export default function DefinitionPage(props) {
+  const [definitions, updateDefinition] = useState([])
   const params = useParams();
+  useEffect(() => {
+    apiCallDef(params.word);
+  }, [])
   console.log('Definition Page', props)
   console.log('match', params.word);
-  const [definitions, updateDefinition] = useState([])
+
 
   const apiCallDef = async (word) => {
     const response = await axios(`https://www.dictionaryapi.com/api/v3/references/collegiate/json/${word}?key=${process.env.REACT_APP_DEF}`)
     console.log(response.data);
     updateDefinition(response.data);
   }
-  apiCallDef(params.word);
   return (
     <div className="definitionPage">
-      <Link to="/"><h1>Eye4Words</h1></Link>
+      {/* <Link to="/"><h1>Eye4Words</h1></Link> */}
+      <NavLink to="/"><h1>Eye4Words</h1></NavLink>
       <h3>{params.word}</h3>
       {definitions.length > 0 ? definitions.map(e =>
-        <div>{e.shortdef[0]}</div>
+        <div key={e.meta.uuid}>{e.shortdef[0]}</div>
       ) : ""}
     </div>
   )
